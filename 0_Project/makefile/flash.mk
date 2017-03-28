cbc2c06f5b829067b3d8b1e94ab773484c126937
mawenke <315261982@qq.com>
2017-03-28 11:47:20 +0800

    correct a error of flash.mk

--- 0_Project/makefile/flash.mk
+++ 0_Project/makefile/flash.mk
@@ -4,7 +4,7 @@ STLINK_PATH     = $(TOP_PATH)/5_Development_Toolchain/stlink
 OPENOCD			= $(TOP_PATH)/5_Development_Toolchain/openocd
 
 ifeq ($(OPENOCD),$(wildcard $(OPENOCD)))
-OPENOCD_PATH    = $(TOP_PATH)/5_Development_Toolchain/openocd/src/
+OPENOCD_PATH    = $(TOP_PATH)/5_Development_Toolchain/openocd/
 else
 OPENOCD_PATH 	=
 endif
@@ -38,24 +38,24 @@ BURN_CONFIG_FILE = stm32f4x
 endif
 
 swd_openocd_flash:$(PROJECT).bin 
-	sudo $(OPENOCD_PATH)openocd -s $(OPENOCD_PATH)/tcl -f interface/jlink.cfg -c "transport select swd" -f target/$(BURN_CONFIG_FILE).cfg -c "init" -c "reset halt" -c "sleep 100" -c "wait_halt 2" -c "flash write_image erase $(PROJECT).bin 0x08000000" -c "sleep 100" -c "verify_image $(PROJECT).bin 0x08000000" -c "sleep 100" -c "reset run" -c shutdown
+	sudo $(OPENOCD_PATH)/src/openocd -s $(OPENOCD_PATH)/tcl -f interface/jlink.cfg -c "transport select swd" -f target/$(BURN_CONFIG_FILE).cfg -c "init" -c "reset halt" -c "sleep 100" -c "wait_halt 2" -c "flash write_image erase $(PROJECT).bin 0x08000000" -c "sleep 100" -c "verify_image $(PROJECT).bin 0x08000000" -c "sleep 100" -c "reset run" -c shutdown
 
 swd_openocd_debug:$(PROJECT).bin 
-	sudo xterm -e $(OPENOCD_PATH)openocd -s $(OPENOCD_PATH)/tcl -f interface/jlink.cfg -c "transport select swd" -f target/$(BURN_CONFIG_FILE).cfg -c "init" -c "halt" -c "reset halt" &
+	sudo xterm -e $(OPENOCD_PATH)/src/openocd -s $(OPENOCD_PATH)/tcl -f interface/jlink.cfg -c "transport select swd" -f target/$(BURN_CONFIG_FILE).cfg -c "init" -c "halt" -c "reset halt" &
 	$(GDB) --eval-command="target remote localhost:3333" $(PROJECT).elf 
 
 swd_openocd_erase:	
-	sudo $(OPENOCD_PATH)openocd -s $(OPENOCD_PATH)/tcl -f interface/jlink.cfg -c "transport select swd" -f target/$(BURN_CONFIG_FILE).cfg  -c "init" -c "reset halt" -c "sleep 100" -c "stm32f4x mass_erase 0" -c "sleep 100" -c shutdown 
+	sudo $(OPENOCD_PATH)/src/openocd -s $(OPENOCD_PATH)/tcl -f interface/jlink.cfg -c "transport select swd" -f target/$(BURN_CONFIG_FILE).cfg  -c "init" -c "reset halt" -c "sleep 100" -c "stm32f4x mass_erase 0" -c "sleep 100" -c shutdown 
 ##################################################################
 stlink_openocd_flash: $(PROJECT).bin
-	sudo $(OPENOCD_PATH)openocd -s $(OPENOCD_PATH)/tcl -f interface/stlink-v2.cfg -f target/$(BURN_CONFIG_FILE)_stlink.cfg -c "init" -c "reset halt" -c "sleep 100" -c "wait_halt 2" -c "flash write_image erase $(PROJECT).bin 0x08000000" -c "sleep 100" -c "verify_image $(PROJECT).bin 0x08000000" -c "sleep 100" -c "reset run" -c shutdown
+	sudo $(OPENOCD_PATH)/src/openocd -s $(OPENOCD_PATH)/tcl -f interface/stlink-v2.cfg -f target/$(BURN_CONFIG_FILE)_stlink.cfg -c "init" -c "reset halt" -c "sleep 100" -c "wait_halt 2" -c "flash write_image erase $(PROJECT).bin 0x08000000" -c "sleep 100" -c "verify_image $(PROJECT).bin 0x08000000" -c "sleep 100" -c "reset run" -c shutdown
 
 stlink_openocd_debug: $(PROJECT).elf stlink_openocd_flash
-	xterm -e $(OPENOCD_PATH)openocd -s $(OPENOCD_PATH)/tcl -f interface/stlink-v2.cfg -f target/$(BURN_CONFIG_FILE)_stlink.cfg -c "init" -c "halt" -c "reset halt" &
+	xterm -e $(OPENOCD_PATH)/src/openocd -s $(OPENOCD_PATH)/tcl -f interface/stlink-v2.cfg -f target/$(BURN_CONFIG_FILE)_stlink.cfg -c "init" -c "halt" -c "reset halt" &
 	$(GDB) --eval-command="target remote localhost:3333" $(PROJECT).elf 
 		
 stlink_openocd_erase:
-	sudo $(OPENOCD_PATH)openocd -s $(OPENOCD_PATH)/tcl -f interface/stlink-v2.cfg -f target/$(BURN_CONFIG_FILE)_stlink.cfg -c "init" -c "reset halt" -c "sleep 100" -c "stm32f4x mass_erase 0" -c "sleep 100" -c shutdown 
+	sudo $(OPENOCD_PATH)/src/openocd -s $(OPENOCD_PATH)/tcl -f interface/stlink-v2.cfg -f target/$(BURN_CONFIG_FILE)_stlink.cfg -c "init" -c "reset halt" -c "sleep 100" -c "stm32f4x mass_erase 0" -c "sleep 100" -c shutdown 
 ##################################################################
 stlink_stlink_flash: $(PROJECT).bin
 	$(STLINK_PATH)/st-flash write $(PROJECT).bin 0x8000000
@@ -72,4 +72,3 @@ stlink_stlink_erase:
 usbttl_bootloader_upload: 
 	sh $(TOOLS_PATH)/upload.sh $(PROJECT).px4	
 ##################################################################
-
