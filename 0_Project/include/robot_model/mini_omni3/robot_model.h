f6487608112d9c344f1e69e63c2bf342e0d4c945
mawenke <31526192@qq.com>
2020-03-28 21:51:29 +0800

    add mini_omni3 robot model

---
+++ 0_Project/include/robot_model/mini_omni3/robot_model.h
@@ -0,0 +1,65 @@
+/***********************************************************************************************************************
+* Copyright (c) Hands Free Team. All rights reserved.
+* Contact:  QQ Exchange Group -- 521037187
+*
+* LICENSING TERMS:
+* The Hands Free is licensed generally under a permissive 3-clause BSD license.
+* Contributions are requiredto be made under the same license.
+*
+* History:
+* <author>      <time>      <version>      <desc>
+*
+* Description:
+***********************************************************************************************************************/
+
+#ifndef ROBOT_MODEL_H
+#define ROBOT_MODEL_H
+
+#include "robot_abstract.h"
+
+class RobotModel : public RobotParameters
+{
+public:
+    RobotModel() : RobotParameters()
+    {
+
+        strcpy(robot_info.robot_name, "mini_omni3");
+        strcpy(robot_info.robot_description ,  "this is a omni3 robot of handsfree");
+
+        motor_para.driver_type = MotorDriver_PWM_AND_IOAB;
+        motor_para.motor_enable_num = 3;
+        motor_para.simulation_model = 0;
+        motor_para.pid_t = 0.02;
+        motor_para.encoder_num  = 1560;
+        motor_para.pwm_max = 5000;
+        motor_para.pwm_dead_zone = 10;
+        motor_para.speed_low_filter = 0.3;
+        motor_para.protect_current = 200;  // 200A means disable current  protect
+        motor_para.pid =  {0.0f , 0.0f , 0.0f , 60.0f , 500.0f , 0.2f , 0.0f , 0.0f , 0.0f};
+
+        chassis_para.type = OMNI3;
+        chassis_para.wheel_radius = 0.0320;
+        chassis_para.body_radius = 0.1025;
+        chassis_para.speed_low_filter = 0.4;
+        chassis_para.imu_fusion_enalbe = 0;
+        chassis_para.control_enable = 1;
+        chassis_para.max_speed_limit.x = 2;
+
+        head_para.type = HFANALOG;
+        head_para.speed_low_filter = 0.3;
+        head_para.range.pitch = 50 * degree_to_radian;
+        head_para.range.yaw = 70 * degree_to_radian;
+        head_para.offset.pitch = 0 *degree_to_radian;
+        head_para.offset.yaw = 0 * degree_to_radian;
+        head_para.id.pitch = 7;
+        head_para.id.yaw = 8;
+        head_para.control_enable = 1;
+
+        arm_para.type = DOBOT2;
+        arm_para.speed_low_filter = 1;
+        arm_para.dof = 4;
+        arm_para.control_enable = 1;
+    }
+};
+
+#endif
